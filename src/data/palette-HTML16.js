33e690fc2268badbe977ecb7fd28e9f85eb22647
Ryan Johnson <rhino.citguy@gmail.com>
2020-06-01 01:49:27 -0500

    update palettes
    - Replace named CSS color values with their HEX equivalent
    - remove SegnoUI palette

--- src/data/palette-HTML16.js
+++ src/data/palette-HTML16.js
@@ -1,23 +1,25 @@
 export default {
   name: 'HTML 4 Web Colors',
-  description: 'Named colors per the HTML 4.01 specification',
-  links: {},
+  description: 'Named colors per the HTML 4.01 specification (a.k.a. CSS Level 1)',
+  links: {
+    'W3C Specification': 'https://www.w3.org/TR/css-color-3/#html4'
+  },
   colors: [
-    { name: 'White', value: 'white' },
-    { name: 'Silver', value: 'silver' },
-    { name: 'Gray', value: 'gray' },
-    { name: 'Black', value: 'black' },
-    { name: 'Red', value: 'red' },
-    { name: 'Maroon', value: 'maroon' },
-    { name: 'Yellow', value: 'yellow' },
-    { name: 'Olive', value: 'olive' },
-    { name: 'Lime', value: 'lime' },
-    { name: 'Green', value: 'green' },
-    { name: 'Aqua', value: 'aqua' },
-    { name: 'Teal', value: 'teal' },
-    { name: 'Blue', value: 'blue' },
-    { name: 'Navy', value: 'navy' },
-    { name: 'Fuchsia', value: 'fuchsia' },
-    { name: 'Purple', value: 'purple' }
+    { name: 'white', value: '#ffffff' },
+    { name: 'silver', value: '#c0c0c0' },
+    { name: 'gray', value: '#808080' },
+    { name: 'black', value: '#000000' },
+    { name: 'red', value: '#ff0000' },
+    { name: 'maroon', value: '#800000' },
+    { name: 'yellow', value: '#ffff00' },
+    { name: 'olive', value: '#808000' },
+    { name: 'lime', value: '#00ff00' },
+    { name: 'green', value: '#008000' },
+    { name: 'aqua', value: '#00ffff' },
+    { name: 'teal', value: '#008080' },
+    { name: 'blue', value: '#0000ff' },
+    { name: 'navy', value: '#000080' },
+    { name: 'fuchsia', value: '#ff00ff' },
+    { name: 'purple', value: '#800080' }
   ]
 }
--- src/data/palettes.js
+++ src/data/palettes.js
@@ -1,11 +1,9 @@
 import X11 from './palette-X11'
 import HTML16 from './palette-HTML16'
 import HelixUI from './palette-HelixUI'
-import SegnoUI from './palette-SegnoUI'
 
 export default [
   HTML16,
   X11,
-  HelixUI,
-  SegnoUI
+  HelixUI
 ]
